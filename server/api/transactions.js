47225631209ea6d51119ae3d5534752435fc1558
Depak Borhara <depakborhara@gmail.com>
2020-01-14 17:38:15 -0500

    Routing for transactions in progress

---
+++ server/api/transactions.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const {Transaction} = require('../db/models')
+module.exports = router
+
+router.get('path', (req, res) => {})
+// Transactions per order route
