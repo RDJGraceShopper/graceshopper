4fae11c21a70030d98a74c7dc40fe40e10ef5d07
Depak Borhara <depakborhara@gmail.com>
2020-01-14 11:01:24 -0500

    User Model updated

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,10 +3,28 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+      notEmpty: true
+    }
+  },
+  address: {
+    type: Sequelize.STRING,
     allowNull: false
+  },
+  tier: {
+    type: Sequelize.INTEGER
   },
   password: {
     type: Sequelize.STRING,
