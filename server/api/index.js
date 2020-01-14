7c19fecc18444848b5df540499c65b023e7726e1
Robert Costello <robertcostello81@gmail.com>
2020-01-14 16:16:26 -0500

    added routes for product, tags, paymentMethod

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,9 @@ module.exports = router
 
 router.use('/users', require('./users'))
 router.use('/orders', require('./orders'))
+router.use('/tags', require('./tags'))
+router.use('/products', require('./products'))
+router.use('/paymentMethods', require('./paymentMethods'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
---
+++ server/api/products.js
@@ -0,0 +1,35 @@
+const router = require('express').Router()
+const {Product, Tag} = require('../db/models')
+const db = require('../db/db')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll()
+    res.json(products)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId)
+    res.json(product)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// router.get('/:productId', async (req, res, next) => {
+//   try {
+//     db.tag-product.findAll({
+//       where: {
+//         pro
+//       } }
+//     )
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+
+module.exports = router
