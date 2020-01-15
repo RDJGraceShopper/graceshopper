a3a8a16f69623a6e1d6ce31314b7250ba496c2f4
jasenvchan <jachan@gwu.edu>
2020-01-15 15:57:17 -0500

    products reducer base created

---
+++ client/store/actionTypes.js
@@ -0,0 +1,18 @@
+// user action types
+export const GOT_ALL_USERS = 'GOT_ALL_USERS'
+export const GOT_SINGLE_USER = 'GOT_SINGLE_USER'
+export const UPDATED_SINGLE_USER = 'UPDATED_SINGLE_USER'
+export const REMOVE_USER = 'REMOVE_USER'
+
+// product action types
+
+export const GOT_ALL_PRODUCTS = 'GOT_ALL_PRODUCTS'
+export const GOT_SINGLE_PRODUCT = 'GOT_SINGLE_PRODUCT'
+export const UPDATED_SINGLE_PRODUCT = 'UPDATED_SINGLE_PRODUCT'
+export const REMOVE_PRODUCT = 'REMOVE_PRODUCT'
+
+// order action types
+
+export const GOT_ALL_ORDERS = 'GOT_ALL_ORDERS'
+export const GOT_SINGLE_ORDER = 'GOT_SINGLE_ORDER'
+export const UPDATED_SINGLE_ORDER = 'UPDATED_SINGLE_ORDER'
