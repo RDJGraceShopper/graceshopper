60d7c9bc28b23cfd51711554a3f9ad65ffcde5f7
jasenvchan <jachan@gwu.edu>
2020-01-21 18:20:13 -0500

    updated cart

--- client/components/Cart/cart.js
+++ client/components/Cart/cart.js
@@ -43,13 +43,13 @@ class Cart extends Component {
             return (
               <li key={product.id}>
                 <h4>{product.name}</h4>
-                <p>Price: {product.orderProduct.price}</p>
+                <p>Price: {product.orderProduct.price / 100}</p>
                 <p>Quantity: {product.orderProduct.quantity}</p>
               </li>
             )
           })}
         </ul>
-        <h2>Order total price: {this.props.openOrder.total}</h2>
+        <h2>Order total price: {this.props.order.total / 100}</h2>
         <button onClick={() => this.submitOrder()}>Submit</button>
       </div>
     )
